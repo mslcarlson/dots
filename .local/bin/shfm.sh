21c88dda99070af62b90c90b9e0ef5d934720144
Matthew Carlson <matt@mcarlson.xyz>
2021-07-10 02:25:12 +0000

    dots

---
+++ .local/bin/shfm.sh
@@ -0,0 +1,58 @@
+#!/bin/sh -e
+#
+# shfm
+
+case "${1}" in
+    # audio
+    *.flac \
+  | *.m4a  \
+  | *.mp3  \
+  | *.ogg  \
+  | *.opus \
+  | *.wav)
+        "${MEDIA_PLAYER}" --no-video "${1}"
+    ;;
+
+    # document
+    *.djvu \
+  | *.epub \
+  | *.pdf)
+        "${READER}" "${1}"
+    ;;
+
+    # gif
+    *.gif)
+        "${MEDIA_PLAYER}" --loop "${1}"
+    ;;
+
+    # image
+    *.jpeg \
+  | *.jpg  \
+  | *.png  \
+  | *.tga  \
+  | *.webp)
+        "${IMG_VIEWER}" "${1}"
+    ;;
+
+    # video
+    *.avi \
+  | *.mkv \
+  | *.mp4 \
+  | *.webm)
+        "${MEDIA_PLAYER}" "${1}"
+    ;;
+
+    # web/vector
+    *.htm   \
+  | *.html  \
+  | *.php   \
+  | *.svg   \
+  | *.xhtml)
+        "${BROWSER}" "${1}"
+    ;;
+
+    # everything else
+    *)
+        "${EDITOR:-vi}" "${1}"
+    ;;
+esac
