6b81d7498fa44def726cb2d8e0bafee930f7276b
Matthew Carlson <matt@mcarlson.xyz>
2021-07-23 22:01:48 -0400

    changed colors around and fixed some aliases

--- .local/bin/wp.sh
+++ .local/bin/wp.sh
@@ -2,7 +2,10 @@
 #
 # wp
 
-WP_DIR="${XDG_DATA_HOME:-${HOME}/.local/share}/wp"
+WP="${XDG_DATA_HOME:-${HOME}/.local/share}/wp"
+COLORS="${XDG_CACHE_HOME:-${HOME}/.cache}/wal/colors.sh"
+DEFAULTBG=259
+XRESOURCES="${XDG_CONFIG_HOME:-${HOME}/.config}/x/xresources"
 
 get_rand_file() {
     flag=1
@@ -55,7 +58,35 @@ check_file () {
 }
 
 wp () {
-    feh --no-fehbg --bg-scale "${1}"
+    ln -sf "${file}" "${WP}"
+    feh --no-fehbg --bg-scale "${WP}"
+    command -v wal >/dev/null && theme
+}
+
+theme() {
+    wal -c && wal -i "${WP}"
+    [ -f "${COLORS}" ] && . "${COLORS}"
+
+    set -- ${color0} ${color1} ${color3} ${color4} ${color5} ${color6} ${color7} ${color8} \
+           ${color9} ${color10} ${color11} ${color12} ${color13} ${color14} ${color15}
+
+    i=0
+    for color in "${@}"; do
+        color="$(printf '%s' "${color}" | tr '[:upper:]' '[:lower:]')"
+        sed -i "s/#define col${i} .*/#define col${i} ${color}/" "${XRESOURCES}"
+        i=$((i+1))
+        [ ${i} -eq 16 ] && break
+    done
+
+    xrdb -merge "${XRESOURCES}"
+
+    kill -s USR1 $(pidof st)
+
+    for tty in $(ls /dev/pts/ | egrep -i '[1-9][0-9]*'); do
+        printf "\033]4;${DEFAULTBG};${color0}\007" > "/dev/pts/${tty}" 2>/dev/null
+    done
+
+    dwmc reload
 }
 
 { [ -d "${1}" ] && get_rand_file "${1}" && flag=${?} ; } || { [ -f "${1}" ] && check_file "${1}" && flag=${?} ; }
