1cb57b543cd8d5e76f8aa257065d8f9a5c74c47b
Matthew Carlson <matt@mcarlson.xyz>
2021-07-29 12:07:18 -0400

    pin script and ytfzf alias

---
+++ .local/bin/pin.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# pin
+
+get_pin() {
+    prompt='Pin'
+
+    # get pin using dmenu and check
+    if pin="$(dmenu -c -p "${prompt}" -P 2>/dev/null)"; then
+        [ -n "${pin}" ] && printf '%s\n' "D ${pin}"
+        printf 'OK\n'
+    # operation cancelled
+    else printf 'ERR 99\n'
+    fi
+}
+
+main() {
+    # export display
+    if [ -z "${DISPLAY}" ]; then
+        DISPLAY=':1'
+        export DISPLAY
+    fi
+
+    # start msg
+    printf 'OK Pleased to meet you\n'
+
+    # ipc
+    while read -r line; do
+        # cmds and data needed to interface with pinentry
+        cmd="$(printf '%s\n' "${line}" | cut -d ' ' -f 1)"
+        data="$(printf '%s\n' "${line}" | cut -d' ' -f 2-)"
+
+        case "${cmd}" in
+            # GETPIN is command pinentry uses for GNUPG pass
+            GETPIN) get_pin       ;;
+            *)      printf 'OK\n' ;;
+        esac
+    done
+}
+
+main "${@}"
